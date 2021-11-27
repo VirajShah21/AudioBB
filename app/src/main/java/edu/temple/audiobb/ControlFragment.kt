3d78c6f566034acbbef929c3f45c16d4ba511b52
Viraj Shah <shahvirajd@gmail.com>
2021-11-27 02:38:51 -0500

    Correctly setup Controller Fragment

--- app/src/main/java/edu/temple/audiobb/ControlFragment.kt
+++ app/src/main/java/edu/temple/audiobb/ControlFragment.kt
@@ -1,10 +1,12 @@
 package edu.temple.audiobb
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,10 +25,11 @@ class ControlFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    }
+
+    fun getBookVM(): SelectedBookViewModel {
+        return ViewModelProvider(requireActivity()).get(SelectedBookViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -34,7 +37,13 @@ class ControlFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false)
+        val resultView = inflater.inflate(R.layout.fragment_control, container, false)
+
+        resultView.findViewById<Button>(R.id.playPause).setOnClickListener {
+
+        }
+
+        return resultView;
     }
 
     companion object {
@@ -50,10 +59,10 @@ class ControlFragment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ControlFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
             }
     }
 }
